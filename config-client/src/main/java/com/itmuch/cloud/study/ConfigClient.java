71adb32ac594d798b48b4186e254f243c7efc152
19511 <123>
2018-08-10 17:23:07 +0800

    init

--- config-client/src/main/java/com/itmuch/cloud/study/ConfigClient.java
+++ config-client/src/main/java/com/itmuch/cloud/study/ConfigClient.java
@@ -3,9 +3,12 @@ package com.itmuch.cloud.study;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author yangyueming
+ */
 @SpringBootApplication
-public class ConfigClientApplication {
+public class ConfigClient {
   public static void main(String[] args) {
-    SpringApplication.run(ConfigClientApplication.class, args);
+    SpringApplication.run(ConfigClient.class, args);
   }
 }
