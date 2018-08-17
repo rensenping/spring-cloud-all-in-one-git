ccaa0c20a5c8bcee8d271490c2437c284fee54f4
19511 <123>
2018-08-17 14:49:55 +0800

    init

---
+++ elk/src/main/java/com/jack/elk/ElkApplication.java
@@ -0,0 +1,15 @@
+package com.jack.elk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yangyueming
+ */
+@SpringBootApplication
+public class ElkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElkApplication.class, args);
+    }
+}
