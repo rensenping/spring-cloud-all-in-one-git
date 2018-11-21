eb762076334b7f02f48bbb5e86f25e55011def0d
yangyueming <123>
2018-11-21 20:17:46 +0800

    init

---
+++ providers/provider-user-simple/src/main/java/com/jack/simple/provider/user/config/JpaConfig.java
@@ -0,0 +1,14 @@
+package com.jack.simple.provider.user.config;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+/**
+ * @author yangyueming
+ */
+@Configuration
+@EntityScan(basePackages={"com.jack.common.entity"})
+@EnableJpaRepositories(basePackages={"com.jack.common.repository"})
+public class JpaConfig {
+}
---
+++ providers/provider-user-simple/src/main/java/com/jack/simple/provider/user/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.jack.simple.provider.user.controller;
+
+import com.jack.common.entity.User;
+import com.jack.common.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * @author yangyueming
+ */
+@RestController
+public class UserController {
+  @Autowired
+  private UserRepository userRepository;
+
+  @GetMapping("/{id}")
+  public User findById(@PathVariable Long id) {
+    return userRepository.findById(id).orElse(null);
+  }
+}
