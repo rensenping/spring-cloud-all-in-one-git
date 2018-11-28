0ac6bf143365ca58277b53273750b4f482bc2bc8
yangyueming <123>
2018-11-28 15:30:57 +0800

    init

--- consumers/consumer-movie-restTemplate/src/main/java/com/jack/consumer/movie/ConsumerMovieRestTemplateApp.java
+++ consumers/consumer-movie-restTemplate/src/main/java/com/jack/consumer/movie/ConsumerMovieRestTemplateApp.java
@@ -1,18 +1,23 @@
-package com.jack.simple.consumer.movie;
+package com.jack.consumer.movie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+/**
+ * @author yangyueming
+ */
+@EnableDiscoveryClient
 @SpringBootApplication
-public class SimpleConsumerMovieApp {
+public class ConsumerMovieRestTemplateApp {
   @Bean
   public RestTemplate restTemplate() {
     return new RestTemplate();
   }
 
   public static void main(String[] args) {
-    SpringApplication.run(SimpleConsumerMovieApp.class, args);
+    SpringApplication.run(ConsumerMovieRestTemplateApp.class, args);
   }
 }
